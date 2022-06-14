4dd5f3bd186ba9ad54de9d0dac76024ea1cc8500
hlongoldman <qml0204@gmail.com>
2022-06-14 14:49:28 +0700

    dashboard

--- src/app/app.menu.component.ts
+++ src/app/app.menu.component.ts
@@ -8,41 +8,66 @@ import { UserService } from './core/services/user.service';
     template: `
         <div class="layout-menu-container">
             <ul class="layout-menu" role="menu" (keydown)="onKeydown($event)">
-                <li app-menu class="layout-menuitem-category" *ngFor="let item of model; let i = index;" [item]="item" [index]="i" [root]="true" role="none">
-                    <div class="layout-menuitem-root-text" [attr.aria-label]="item.label">{{item.label | translate}}</div>
+                <li
+                    app-menu
+                    class="layout-menuitem-category"
+                    *ngFor="let item of model; let i = index"
+                    [item]="item"
+                    [index]="i"
+                    [root]="true"
+                    role="none"
+                >
+                    <div
+                        class="layout-menuitem-root-text"
+                        [attr.aria-label]="item.label"
+                    >
+                        {{ item.label | translate }}
+                    </div>
                     <ul role="menu">
-                        <li app-menuitem *ngFor="let child of item.items" [item]="child" [index]="i" role="none"></li>
+                        <li
+                            app-menuitem
+                            *ngFor="let child of item.items"
+                            [item]="child"
+                            [index]="i"
+                            role="none"
+                        ></li>
                     </ul>
                 </li>
             </ul>
         </div>
-    `
+    `,
 })
 export class AppMenuComponent implements OnInit {
-
     model: any[];
     currentUser: User;
     roleActions: number[];
 
     constructor(
         public appMain: AppMainComponent,
-        private userService: UserService,
-    ) { }
+        private userService: UserService
+    ) {}
 
     ngOnInit() {
         this.userService._user$.subscribe({
             next: (user) => {
                 this.currentUser = user;
-                this.roleActions = user.userRole.roleActions.map(e => e.actionId);
-            }
+                this.roleActions = user.userRole.roleActions.map(
+                    (e) => e.actionId
+                );
+            },
         });
 
         this.model = [
             // {
-            //     label: 'Home',
+            //     label: 'Dashboard',
             //     items: [
-            //         { label: 'Analysis', icon: 'pi pi-fw pi-chart-pie', routerLink: ['/'] }
-            //     ]
+            //         {
+            //             label: 'Analysis',
+            //             icon: 'pi pi-fw pi-chart-pie',
+            //             routerLink: ['/dashboard'],
+            //             show: true,
+            //         },
+            //     ],
             // },
             {
                 label: 'Administrator',
@@ -51,45 +76,45 @@ export class AppMenuComponent implements OnInit {
                         label: 'Department',
                         icon: 'pi pi-fw pi-sitemap',
                         routerLink: ['/department'],
-                        show: this.roleActions.includes(1203)
+                        show: this.roleActions.includes(1203),
                     },
                     {
                         label: 'Employees',
                         icon: 'pi pi-fw pi-users',
                         routerLink: ['/employees'],
-                        show: this.roleActions.includes(100)
+                        show: this.roleActions.includes(100),
                     },
                     {
                         label: 'Role',
                         icon: 'pi pi-fw pi-sliders-v',
                         routerLink: ['/permissions'],
-                        show: this.roleActions.includes(800)
+                        show: this.roleActions.includes(800),
                     },
                     {
                         label: 'Service',
                         icon: 'pi pi-fw pi-server',
                         routerLink: ['/services'],
-                        show: this.roleActions.includes(300)
+                        show: this.roleActions.includes(300),
                     },
                     {
                         label: 'Project',
                         icon: 'pi pi-fw pi-briefcase',
                         routerLink: ['/projects'],
-                        show: this.roleActions.includes(400)
+                        show: this.roleActions.includes(400),
                     },
                     {
                         label: 'Category',
                         icon: 'pi pi-fw pi-database',
                         routerLink: ['/category'],
-                        show: this.roleActions.includes(1303)
+                        show: this.roleActions.includes(1303),
                     },
                     {
                         label: 'Code',
                         icon: 'pi pi-fw pi-code',
                         routerLink: ['/code'],
-                        show: this.roleActions.includes(1403)
+                        show: this.roleActions.includes(1403),
                     },
-                ]
+                ],
             },
             {
                 label: 'Work',
@@ -98,15 +123,19 @@ export class AppMenuComponent implements OnInit {
                         label: 'Campaign',
                         icon: 'pi pi-fw pi-chart-line',
                         routerLink: ['/campaigns'],
-                        show: this.roleActions.includes(600) || this.roleActions.includes(601)
+                        show:
+                            this.roleActions.includes(600) ||
+                            this.roleActions.includes(601),
                     },
                     {
                         label: 'Campaign_services',
                         icon: 'pi pi-fw pi-money-bill',
                         routerLink: ['/campaign-services'],
-                        show: this.roleActions.includes(700) || this.roleActions.includes(701)
+                        show:
+                            this.roleActions.includes(700) ||
+                            this.roleActions.includes(701),
                     },
-                ]
+                ],
             },
             {
                 label: 'Finance',
@@ -115,9 +144,9 @@ export class AppMenuComponent implements OnInit {
                         label: 'Transaction',
                         icon: 'pi pi-fw pi-wallet',
                         routerLink: ['/transactions'],
-                        show: this.roleActions.includes(1000)
+                        show: this.roleActions.includes(1000),
                     },
-                ]
+                ],
             },
             // {
             //     label: 'Hierarchy',
@@ -165,7 +194,7 @@ export class AppMenuComponent implements OnInit {
     }
 
     onKeydown(event: KeyboardEvent) {
-        const nodeElement = (<HTMLDivElement>event.target);
+        const nodeElement = <HTMLDivElement>event.target;
         if (event.code === 'Enter' || event.code === 'Space') {
             nodeElement.click();
             event.preventDefault();
